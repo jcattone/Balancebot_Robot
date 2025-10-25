44b0ceb258cb63fb7170e719cacfd9123173ff4b
Jeremy Cattone <jcattone@gmail.com>
2025-10-24 21:50:51 -0700

    Initial commit - WIP porting to use EspNowRemote

---
+++ BalanceBot_Robot.ino
@@ -0,0 +1,539 @@
+// Pinout:
+//   I2C SDA: 21
+//   I2C SCL: 22
+//   Analog Joystick X/Y: 25, 26
+// Devices
+//   MPU-6050: I2C
+//     22uF cap across power
+//   SSD1306 128x32: I2C
+//   DRV8833
+//     Motor 1: Pins 18, 19
+//   10k pull-up resistors on both I2C lines
+
+// The Adafruit library gives us basic MPU-6050 accel and gyro data
+#include <Adafruit_MPU6050.h>
+
+// We'll use an SSD1306 128x32 display in lieu of serial output,
+// which can interfere with the I2C bus
+#include <Adafruit_SSD1306.h>
+
+// AHRS algorithms produce a stablized estimation of bearing from a 6-axis (or 9-axis) IMU.
+// The Mahony algorithm is computationally inexpensive, and seems to provide sufficiently
+// good results (better than a basic complementary filter)
+// Other algos (Madgwick, NXP) were difficult to tune, and were not delivering better data
+// for the purpose of a balance bot
+#include "Adafruit_AHRS_Mahony.h"
+
+#include <esp32-hal-ledc.h>
+
+// ESP-NOW control
+#include <esp_now.h>
+#include <esp_wifi.h>
+#include <WiFi.h>
+
+#include "EspNowRemote.h"
+#include "EspNowRemote_Events.h"
+using namespace EspNowRemote;
+RmtBase* remote = EspNowRemote::MakeController();
+joystick_state_t g_joystick_state = {};
+
+// #define ENABLE_SERVO 1
+// #include <ESP32Servo.h>
+// Servo myservo;
+// #define SERVO_PIN 32
+
+// Initialize the IMU filter weights:
+// Kp ~= trust accel data (gravity vector) to correct gyro drift
+//   High Kp = correct quickly, but linear acceleration (i.e., translation) can be misread as orientation change
+//   Low Kp = trust the gyro more, can be sluggish to respond
+//   i.e., choose lowest Kp with acceptable linear acceleration characteristics
+// Ki ~= correction speed for gyro bias (from accel gravity reference)
+//   High Ki = aggressively remove bias, but can become sluggish.  In practice, overshoot is observed.
+//   Low Ki = may allow bias to persistently affect the output
+// In practice, I'm seeing eyeball-reasonable results with Kp=10..25, and Ki=0..5
+Adafruit_Mahony filter(20, 4);  // ...(float prop_gain, float int_gain) // Kp, Ki
+
+// The IMU instance itself
+Adafruit_MPU6050 mpu;
+
+// The display... Assumed to be on the default I2C pins
+Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire);
+
+#define SCREEN_WIDTH 128      // OLED display width, in pixels
+#define SCREEN_CHAR_WIDTH 21  // 5+1 pixel font width
+#define SCREEN_HEIGHT 32      // OLED display height, in pixels
+#define SCREEN_HEIGHT_ROWS 4  // 7+1 pixel font height
+#define OLED_RESET -1         // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C   ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+char textBuffer[SCREEN_HEIGHT_ROWS][SCREEN_CHAR_WIDTH];
+
+// With each update, the orientation will be captured in gOrientation
+struct OrientationAngles {
+  float pitch;
+  float roll;
+  float yaw;
+} gOrientation;
+
+// To support multiple config modes...
+enum eConfigMode {
+  ePwmMin,
+  ePwmMax,
+  ePwmFreq,
+  ePidKp,
+  ePidKi,
+  ePidKd,
+  eHBridgeIdle,
+  eFusionKp,
+  eFusionKi,
+  eMaxConfigMode
+};
+eConfigMode gCurrentConfigMode = eFusionKp;
+
+enum eHBridgeIdleMode {
+  eBraking,
+  eCoasting,
+  eIdleUnknown
+};
+eHBridgeIdleMode gHBridgeIdleMode = eBraking;
+
+enum RelativeDirection {
+  eStopped,
+  eForward,
+  eBackward,
+  eDirectionUnknown
+};
+
+#define MOTOR_PIN_1 18
+#define MOTOR_PIN_2 19
+
+
+int gPwmMinDuty = 20;
+int gPwmMaxDuty = 160;
+int gPwmCurrentDuty = 0;
+int gPwmDutyMagnitude = 0;
+int gPwmFreq = 25;
+
+float gPidKp = 1.0f;
+float gPidKi = 0.5f;
+float gPidKd = 0.0f;
+
+void OnDataSent(const esp_now_send_info_t* tx_info, esp_now_send_status_t sendStatus) {
+}
+void IRAM_ATTR OnDataRecv(const esp_now_recv_info_t* esp_now_info, const uint8_t* data, int data_len) {
+  remote->HandleDataReceived(esp_now_info, data, data_len);
+}
+
+unsigned long last_hid_input_timestamp = 0;
+bool OnControllerMessage(uint8_t msg_type, const uint8_t* data, int data_len) {
+  switch (msg_type) {
+    case EspNowRemote::MSGTYPE_SELF_STATUS:
+      // Loopback status from the local remote instance... not from the controller.
+      memcpy(textBuffer[1], data, data_len);
+      textBuffer[1][data_len] = 0;
+      break;
+    case MSGTYPE_RMT_JOYSTICK:
+      memcpy(&g_joystick_state, data, data_len);
+      last_hid_input_timestamp = micros();
+  }
+  return true;
+}
+
+void setup() {
+  Serial.begin(38400);
+  // while (!Serial);
+  Serial.println("MPU6050 demo");
+
+  // ESP-NOW control
+  WiFi.persistent(false);
+  WiFi.mode(WIFI_STA);
+  esp_wifi_set_promiscuous(true);  // Required to set a specific channel
+  esp_wifi_set_channel(1 /* channel */, WIFI_SECOND_CHAN_NONE);
+  esp_wifi_set_promiscuous(false);
+
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // TODO: initMotors()
+  pinMode(MOTOR_PIN_1, OUTPUT);
+  pinMode(MOTOR_PIN_2, OUTPUT);
+
+  analogWriteFrequency(MOTOR_PIN_1, gPwmFreq);
+  analogWriteFrequency(MOTOR_PIN_2, gPwmFreq);
+  // Initializing the motor pins uniformly solves the 'jerk on startup' problem
+  if (gHBridgeIdleMode == eBraking) {
+    analogWrite(MOTOR_PIN_1, 1);
+    analogWrite(MOTOR_PIN_2, 1);
+  } else {
+    analogWrite(MOTOR_PIN_1, 0);
+    analogWrite(MOTOR_PIN_2, 0);
+  }
+
+  initDisplay();
+  initImu();
+
+  esp_now_register_send_cb(OnDataSent);
+  esp_now_register_recv_cb(OnDataRecv);
+
+  remote->Setup();
+  remote->RegisterMsgHandler(OnControllerMessage);
+  remote->Start();
+}
+
+void loop() {
+  remote->Loop();
+
+  handleInput();
+  updateOrientation();
+  updateDisplay();
+  updateMotors();
+}
+
+void updateMotors() {
+  static unsigned long lastUpdate = 0;
+
+  static int lastPos = 90;
+  unsigned long now = millis();
+  if (now - lastUpdate < 20)
+    return;
+  lastUpdate = now;
+
+  // PID per-update inputs
+  int desiredAngle = 0.0f;
+  // [-90, -90] generally speaking (technically, can go to +/- 180)
+  float currentAngle = gOrientation.roll;
+
+  // Calculate dT in seconds (the actual time unit is arbitrary, as long as we're consistent)
+  static unsigned long lastSampleTime = 0;
+  static bool isFirstSample = true;
+  float deltaTSec = static_cast<float>(now - lastSampleTime) / 1000.0f;
+  lastSampleTime = now;
+  // The first sample is used only to set the sample time, so that
+  // the next sample (the first real one) can be evaluated with an
+  // accurate inter-sample deltaT.
+  if (isFirstSample) {
+    isFirstSample = false;
+    return;
+  }
+
+  // How far off are we?
+  // TODO: currentAngleAdjusted is where we could subtract the
+  // angle error predicted from the current acceleration.
+  // This is sometimes known as 'covariance' adjustment,
+  // compensating for the acceleration's impact on the
+  // apparent gravity vector.
+  float currentAngleAdjusted = currentAngle;
+  // TODO: TBD whether this error has the correct sign - we may
+  // need to flip this to change the direction of the feedback.
+  float errorAngle = currentAngleAdjusted - desiredAngle;
+
+  static int lastErrorSign = 0;
+  int currentErrorSign = std::signbit(errorAngle);
+
+  // P: [-90, 90] typical
+  float P = errorAngle;
+
+  // I: ?
+  static float errorIntegral = 0.0;
+
+  // Clear the integral when we cross the setpoint (don't carry windup across a stable threshold).
+  // Note that this doesn't account for inertia - we might wind up crossing rapidly - this suggests
+  // a use for the derivative term.
+  if (currentErrorSign != lastErrorSign)
+    errorIntegral = 0;
+
+  // Only accumulate error when we aren't saturated
+  if (std::abs(gPwmCurrentDuty) < gPwmMaxDuty) {
+    errorIntegral += errorAngle * deltaTSec;
+  }
+
+  float I = errorIntegral;
+
+  // D=D
+  // Identify the rate at which the error is changing
+  // As this may be very noisy, a low-pass (short IIR) filtered
+  // version is also available.  Maybe take the derivative as the
+  // difference between two different IIR periods?
+  // Note: the Kalman filter is supposed to predict the current delta,
+  // rather than work from historical data - a theoretical improvement
+  // at additional complexity.
+  static float lastErrorAngle = 0.0f;
+  static float errorDeltaPerSecondIIR = 0.0f;
+  static float errorDeltaPerSecondIIRWeight = 0.60f;
+  float errorDeltaPerSecond = (errorAngle - lastErrorAngle) / deltaTSec;
+  // IIR; weight the accumulator heavily, and the new value lightly.
+  errorDeltaPerSecondIIR = errorDeltaPerSecondIIRWeight * errorDeltaPerSecondIIR + (1.0f - errorDeltaPerSecondIIRWeight) * errorDeltaPerSecond;
+  //Serial.printf("eA:%.1f,lea:%.1f,dT:%.3f,edps:%.1f,edpsIIR:%.1f\n", errorAngle, lastErrorAngle, deltaTSec, errorDeltaPerSecond, errorDeltaPerSecondIIR);
+  lastErrorAngle = errorAngle;
+  float D = errorDeltaPerSecondIIR;
+
+  float PIDOutput = gPidKp * P + gPidKi * I + gPidKd * D;
+
+  // Note that we want the acceleration (not speed) to be modulated by the PID output.
+  // TODO: Note that this is an integer, limiting fine-grained control especially close to 0
+  int rawAccel = constrain(PIDOutput, -255.0f, 255.0f);
+
+  // Compress the speed range to the PWM min/max range
+  //int constrainedAccel = map((int)std::abs(rawAccel), -gPwmMaxDuty, gPwmMaxDuty, gPwmMinDuty, gPwmMaxDuty);
+
+  // Serial.printf("cA:%.1f,errA:%.1f,P:%.1f,I:%.1f,D:%.1f,PID:%.1f\n", currentAngle, errorAngle, P, I, D, PIDOutput);
+
+  // Attenuate the PWM output on startup to prevent noisy output
+  static int startupPwmAttenuation = 0;
+  int constrainedAccel = constrain(rawAccel, -startupPwmAttenuation, startupPwmAttenuation);
+  if (startupPwmAttenuation < 255)
+    ++startupPwmAttenuation;
+
+  // Adjust the speed by the desired relative acceleration, constraining the duty cycle to the PWM limits.
+  // Note that this can be negative, to indicate a reversed direction.
+  gPwmCurrentDuty = constrain(gPwmCurrentDuty + constrainedAccel, -gPwmMaxDuty, gPwmMaxDuty);
+
+  // Now compress the output to eliminate the dead zone.
+  // TODO: more nuanced mapping (e.g., near-zero dead-zone + fast-ramp)
+  gPwmDutyMagnitude = map(std::abs(gPwmCurrentDuty), 0, gPwmMaxDuty, gPwmMinDuty, gPwmMaxDuty);
+
+  // Depending upon direction, the DRV8833 needs different pins driven.
+  bool m1Driven;
+  int m1 = 0, m2 = 0;
+  if (gPwmCurrentDuty < 0) {
+    // Note: for fast-decay (coasting), one pin is pulled low, the other is high at the desired duty.
+    // For slow-decay (braking), one pin is pulled high, the other is low at the desired duty.
+    // Because analogWrite specified the high-side duty cycle, slow-decay mode means we invert both
+    // of the duty cycles (i.e., 255 on on to hold it high, and 255-duty on the other, so that it is
+    // LOW at the desired duty)
+    m2 = gPwmDutyMagnitude;
+    m1Driven = false;
+  } else {
+    m1 = gPwmDutyMagnitude;
+    m1Driven = true;
+  }
+
+  // Adjust for braking (slow-decay) / coasting (fast-decay) drive modes.
+  if (gHBridgeIdleMode == eBraking) {
+    // To invert the H-Bridge input, invert the levels and also
+    // swap the driven IO to maintain direction.
+    int m1Temp = m1;
+    m1 = 255 - m2;
+    m2 = 255 - m1Temp;
+    m1Driven = !m1Driven;
+  }
+
+  // Update the motor PWM.
+  analogWrite(MOTOR_PIN_1, m1);
+  analogWrite(MOTOR_PIN_2, m2);
+}
+
+void initDisplay() {
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {  // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ;  // Don't proceed, loop forever
+  }
+  display.display();
+  delay(20);
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setRotation(0);
+}
+
+void initImu() {
+  if (!mpu.begin()) {
+    Serial.println("MPU-6050 init failed");
+    while (1)
+      yield();
+  }
+  Serial.println("Found a MPU-6050 sensor");
+
+  // Param: samples per second
+  filter.begin(100);
+  // THen:
+  // filter.updateIMU(gx/y/z, ax/y/z, [optional dT]) // DPS (deg. per sec) / Gs
+  // getRoll/Pitch/Yaw(), getGravityVector()
+  // or: getQuaternion
+}
+
+void handleInput() {
+  static unsigned long lastUpdate = 0;
+  static bool isDebouncing = false;
+  unsigned int now = millis();
+  if (now - lastUpdate < 20)
+    return;
+  lastUpdate = now;
+  int joystickY = 2048, joystickX = 2048;
+
+  // USE_ESPNOW
+  static unsigned long last_hid_message_processed = 0;
+  if (last_hid_input_timestamp > last_hid_message_processed) {
+    last_hid_message_processed = last_hid_input_timestamp;
+    if (g_joystick_state.count > 0) {
+      switch (g_joystick_state.joystickDirection) {
+        case JOYSTICK_UP:
+          joystickY = 4096;
+          break;
+        case JOYSTICK_DOWN:
+          joystickY = 0;
+          break;
+        case JOYSTICK_RIGHT:
+          joystickX = 4096;
+          break;
+        case JOYSTICK_LEFT:
+          joystickX = 0;
+          break;
+      }
+      // 'consume' the event
+      joystick_state.count = 0
+    }
+    // ; // last_hid_input_timestamp
+  }
+
+  // Config mode
+  if (isDebouncing) {
+    if (joystickY > 1000 && joystickY < 3000)
+      isDebouncing = false;
+  } else {
+    if (joystickY < 100) {
+      if (gCurrentConfigMode == 0)
+        gCurrentConfigMode = (eConfigMode)(eMaxConfigMode);
+      gCurrentConfigMode = (eConfigMode)(gCurrentConfigMode - 1);
+      isDebouncing = true;
+    } else if (joystickY > 3900) {
+      gCurrentConfigMode = (eConfigMode)(gCurrentConfigMode + 1);
+      if (gCurrentConfigMode == eMaxConfigMode)
+        gCurrentConfigMode = (eConfigMode)0;
+      isDebouncing = true;
+    }
+  }
+
+  // Actual config
+  bool up = joystickX > 3900;
+  bool down = joystickX < 100;
+  switch (gCurrentConfigMode) {
+    case eFusionKp:
+      if (up) filter.setKp(min(100.0, filter.getKp() + 0.1));
+      else if (down) filter.setKp(max(0.0, filter.getKp() - 0.1));
+      break;
+    case eFusionKi:
+      if (up) filter.setKi(min(100.0, filter.getKi() + 0.1));
+      else if (down) filter.setKi(max(0.0, filter.getKi() - 0.1));
+      break;
+    case ePwmMin:
+      if (up) gPwmMinDuty = min(gPwmMaxDuty, gPwmMinDuty + 1);
+      else if (down) gPwmMinDuty = max(0, gPwmMinDuty - 1);
+      break;
+    case ePwmMax:
+      if (up) gPwmMaxDuty = min(255, gPwmMaxDuty + 1);
+      else if (down) gPwmMaxDuty = max(gPwmMinDuty, gPwmMaxDuty - 1);
+      break;
+    case ePwmFreq:
+      if (up) gPwmFreq = min(40000, max(gPwmFreq + 1, (int)(gPwmFreq * 1.1)));
+      else if (down) gPwmFreq = max(10, min(gPwmFreq - 1, (int)(gPwmFreq / 1.1)));
+      if (up || down) {
+        analogWriteFrequency(MOTOR_PIN_1, gPwmFreq);
+        analogWriteFrequency(MOTOR_PIN_2, gPwmFreq);
+      }
+      break;
+    case eHBridgeIdle:
+      if (up)
+        gHBridgeIdleMode = eCoasting;
+      else if (down)
+        gHBridgeIdleMode = eBraking;
+      break;
+    case ePidKp:
+      adjustPidK(&gPidKp, up ? 0.01f : (down ? -0.01f : 0.0f));
+      break;
+    case ePidKi:
+      adjustPidK(&gPidKi, up ? 0.01f : (down ? -0.01f : 0.0f));
+      break;
+    case ePidKd:
+      adjustPidK(&gPidKd, up ? 0.01f : (down ? -0.01f : 0.0f));
+      break;
+  }
+}
+
+void adjustPidK(float* f, float delta) {
+  *f += delta;
+  if (*f < 0.0f)
+    *f = 0.0f;
+}
+
+void updateDisplay() {
+  static unsigned long lastUpdate = 0;
+  unsigned int now = millis();
+  if (now - lastUpdate < 10)
+    return;
+  lastUpdate = now;
+
+  display.clearDisplay();
+
+  // Rows 1-3: Orientation
+  display.setCursor(0, 0);
+  display.printf("Roll:%.2f\nPitch:%.2f\nYaw:%.2f", gOrientation.roll, gOrientation.pitch, gOrientation.yaw);
+
+  // Row 4: current config
+  display.setCursor(0, 3 * 8);
+  switch (gCurrentConfigMode) {
+    case eFusionKp:
+      display.printf("Kp=[%.1f] Ki=%.1f", filter.getKp(), filter.getKi());
+      break;
+    case eFusionKi:
+      display.printf("Kp=%.1f Ki=[%.1f]", filter.getKp(), filter.getKi());
+      break;
+    case ePwmMin:
+      display.printf("pwm=[%d]-%d (%d)", gPwmMinDuty, gPwmMaxDuty, gPwmCurrentDuty);
+      break;
+    case ePwmMax:
+      display.printf("pwm=%d-[%d] (%d)", gPwmMinDuty, gPwmMaxDuty, gPwmCurrentDuty);
+      break;
+    case ePwmFreq:
+      display.printf("pwmFreq=[%d] (%d)", gPwmFreq, gPwmCurrentDuty);
+      break;
+    case eHBridgeIdle:
+      if (gHBridgeIdleMode == eBraking)
+        display.printf("Idle=[B] /  C ");
+      else
+        display.printf("Idle= B  / [C]");
+      break;
+    case ePidKp:
+      display.printf("PID [%.2f] %.2f %.2f", gPidKp, gPidKi, gPidKd);
+      break;
+    case ePidKi:
+      display.printf("PID %.2f [%.2f] %.2f", gPidKp, gPidKi, gPidKd);
+      break;
+    case ePidKd:
+      display.printf("PID %.2f %.2f [%.2f]", gPidKp, gPidKi, gPidKd);
+      break;
+  }
+
+  display.display();
+}
+
+void updateOrientation() {
+  static unsigned long lastUpdate = 0;
+  unsigned int now = millis();
+  if (now - lastUpdate < 10)
+    return;
+  lastUpdate = now;
+
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  // Update in degrees-per-second and gravities
+  filter.update(
+    // From radians-per-second to degrees-per-second
+    g.gyro.x * 180.0 / M_PI,
+    g.gyro.y * 180.0 / M_PI,
+    g.gyro.z * 180.0 / M_PI,
+    // From m/s to gravities
+    a.acceleration.x / 9.81,
+    a.acceleration.y / 9.81,
+    a.acceleration.z / 9.81,
+    0.0,
+    0.0,
+    0.0);
+
+  gOrientation.roll = filter.getRoll();
+  gOrientation.pitch = filter.getPitch();
+  gOrientation.yaw = filter.getYaw();
+}
---
+++ scratch.h
@@ -0,0 +1,152 @@
+#if false
+
+  // rawSpeed is a number strictly within the range [0, 255]
+  // Here, we use the roll as the speed, approaching full speed as we approach 90 degrees.
+  // TODO: In practice, we would need to ramp much earlier to recover our balance
+  // TODO: There's also a nonlinear (1/sin?) relation between angle and speed, as
+  //       angles near to upright (0 degrees roll) need very little correction, but
+  //       recovering from angles > 20 degrees or so needs significant acceleration.
+  //       Note that it's actually accel needed to recover from a tip, not speed...
+  //       Do we want to have an input mode that focuses on accel, or rely on the
+  //       external source to compute accel?
+  // TODO: Also note that to recover balance, we need to overshoot or otherwise
+  //       integrate error rapidly, ensuring that we can obtain a neutral position
+  //       before we max out speed and accel.
+  // We get back to the root of PID... we really want to go from
+  // (desired angle, current angle, error) to speed, with the error integration
+  // providing the overshoot.
+  // Ok, let's focus explicitly on a balance controller
+  // Misc. note: to start moving from still, we actually need to reverse a bit.
+  // this should fall out naturally, as 'move forward' actually translates to
+  // 'maintain an N degree angle to accel, until we're at speed'.  Without an
+  // encoder, we can target having the desired speed at angle 0, and we want
+  // to keep the speed well under our max, so that we always have accel left
+  // to balance. We might even switch to coasting mode for a boost if needed.
+
+  int desiredAngle = 0.0f;
+  float currentAngle = gOrientation.roll;
+  // TODO: Subtract the angle error expected from the current acceleration
+  float currentAngleAdjusted = currentAngle;
+  // How far off are we?
+  float errorAngle = currentAngleAdjusted - desiredAngle;
+  float Kp = 1.0f;
+  float Ki = 0.1f;
+  float Kd = 0.0f;
+
+
+  int rawSpeed = constrain(map((int)std::abs(gOrientation.roll), 0, 90, 0, 255), 0, 255);
+
+  RelativeDirection direction = rawSpeed < 0.0f ? eBackward : eForward;
+
+  // Map the speed to the PWM min/max range
+  int constrainedSpeed = constrain(map((int)std::abs(rawSpeed), 0, 90, gPwmMinDuty, gPwmMaxDuty), gPwmMinDuty, gPwmMaxDuty);
+  gPwmDutyCurrent = constrain(map((int)-gOrientation.roll, 0, 90, gPwmMinDuty, gPwmMaxDuty), gPwmMinDuty, gPwmMaxDuty);
+
+  int m1 = 0, m2 = 0;
+  if (gOrientation.roll < 0) {
+    // Note: for fast-decay (coasting), one pin is pulled low, the other is high at the desired duty.
+    // For slow-decay (braking), one pin is pulled high, the other is low at the desired duty.
+    // Because analogWrite specified the high-side duty cycle, slow-decay mode means we invert both
+    // of the duty cycles (i.e., 255 on on to hold it high, and 255-duty on the other, so that it is
+    // LOW at the desired duty)
+    direction = eForward;
+    m2 = constrain(map((int)-gOrientation.roll, 0, 90, gPwmMinDuty, gPwmMaxDuty), gPwmMinDuty, gPwmMaxDuty);
+  } else {
+    direction = eBackward;
+    m1 = constrain(map((int)gOrientation.roll, 0, 90, gPwmMinDuty, gPwmMaxDuty), gPwmMinDuty, gPwmMaxDuty);
+  }
+
+  // Adjust for braking (slow-decay) / coasting (fast-decay) drive modes.
+  if (gHBridgeIdleMode == eBraking) {
+    // To invert the H-Bridge input, invert the levels and also
+    // swap the driven IO to maintain direction.
+    int m1Temp = m1;
+    m1 = 255 - m2;
+    m2 = 255 - m1Temp;
+  }
+
+  static int startupPwmAttenuation = 0;
+  m1 = constrain(m1, 0, startupPwmAttenuation);
+  m2 = constrain(m2, 0, startupPwmAttenuation);
+  if (startupPwmAttenuation < 255)
+    ++startupPwmAttenuation;
+
+  static float m1IIR = 0.0f;
+  static float m2IIR = 0.0f;
+  // We want to prevent a sudden jump from stopped to high duty
+  // The IIR is supposed to smooth the ramp
+  // But if we swap braking/coasting, or if we reverse direction,
+  // the values both start blending at the same time.
+  // If we allow 0/255 to rail the IIR, then we're no longer protecting
+  // against the inrush.
+  // We could say if an input is 0/255, lock the other to the same rail (i.e., an off state)
+  // However, that would have to be if the value CHANGED and was 0/255, otherwise we'd keep resetting...
+  // We also don't want to suddenly stop if changing modes...
+  // Maybe we just need an initial allowed-difference window?
+  // Could also track the braking mode, and reset when that changes?  Kinda defeats the purpose of toggling though... can't compare betwwen the two.
+  // Maybe if the braking mode changes, or if the direction changes, the IIR is reset to current immediately.
+  // Effectively, if the PWM-driven pin changes, we reset.
+
+  static eHBridgeIdleMode lastIdle = eIdleUnknown;
+  static RelativeDirection lastDirection = eDirectionUnknown;
+  // If there was a change in direction or idle mode, assume that the
+  // motor speed is consistent?
+  // TODO: This still isn't quite correct - starting at 90 degrees would have high in-rush
+  if (lastIdle != eIdleUnknown && lastDirection != eDirectionUnknown && (lastIdle != gHBridgeIdleMode || lastDirection != direction)) {
+    m1IIR = m1;
+    m2IIR = m2;
+  } else {
+    m1IIR = m1IIR * gMotorIIRWeight + (1.0f - gMotorIIRWeight) * m1;
+    m2IIR = m2IIR * gMotorIIRWeight + (1.0f - gMotorIIRWeight) * m2;
+  }
+  lastDirection = direction;
+  lastIdle = gHBridgeIdleMode;
+
+  m1 = (int)m1IIR;
+  m2 = (int)m2IIR;
+  analogWrite(MOTOR_PIN_1, m1);
+  analogWrite(MOTOR_PIN_2, m2);
+
+  // Central dead zone: 0=0
+  // For TT motor...
+  // PWM Duty ramps:
+  //   Running
+  //     Has a Min(10), Max(5/8 * 255=160)
+  //   Breaking (small-magnitude, 0-10?)
+  //     0-Min over short range
+  //   Enable true idle, but ramp quickly to functional power
+  // PWM trending:
+  //   Compare now to short IIR - get rough trend and magnitude up or down
+  // PWM Freq TARGET: 1.0 * duty
+  //   No adjustments for now...
+  // PWM application:
+  //   IIR (allows accel to remain slightly trailing / low on the freq)
+  //   Quantize: change in steps of 1.2
+
+
+  // TODO: Do we want an explicit dead zone (very small center window with no motor movement)?
+  // TODO: When starting motion, give a little 'boost' and then decay to the actual?
+  // TODO: Always allow 0=0 - perhaps have 0 be a true dead zone, and provide a very short ramp to the min PWM, then ramp linearly?
+  // NOTE: Seeing a very functional PWM freq around 20-25Hz.  Jerky, but performs well at low power.
+  // Delivered power/speed falls off as PWM freq increases, even to only a few 100 Hz.
+  // Slow decay mode (high-default, pwm cycles low) improves low-speed performance
+  // PWM freq benefits from speed dependence; low speed (20-30 Hz) works well to get started.
+  // Lower than that (< 20) is really choppy.  As speed ramps, increase pwn freq to 80-100, or even a bit higher.
+  // Min duty can absorb a dead zone.  At low freq (20Hz), dead zone is roughly 0-10 when already stopped.
+  // At higher freq, dead zone is larger - 30-60, depending upon frequency.
+  // But if min is absolute, there's chatter even when 'stopped'.
+  // Propose: Allow ramp to 0, but fast-ramp to the min pwm over just a few steps, then follow linear.
+  // i.e., take minimum of fast ramp or linear, and combine that with a freq 20 <= 1.5*pwmLevel <= 80,
+  // with an adaptive (short IIR?) when ramping up, to avoid rapid toggling (each change to the freq
+  // seems to start a new PWM cycle).  Perhaps also adapt the freq to the power needs - ramping down
+  // can rapidly decay the frequency, but ramping up quickly may be biased toward a slower frequency to
+  // develop more power until at speed.
+
+
+  // auto pos = max(0.0, min(180.0, gOrientation.roll + 90.0));  //map(gOrientation.roll, -180, 180, 0 180)
+  // if (lastPos != pos) {
+  //   myservo.write(pos);
+  //   lastPos = pos;
+  // }
+
+#endif
